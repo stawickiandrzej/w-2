453fd7305e89ff0ba852d3dd903cf32ec8821a0d
stawickiandrzej <56775409+stawickiandrzej@users.noreply.github.com>
2019-10-22 18:16:38 +0200

    ,

---
+++ 1.py
@@ -0,0 +1,12 @@
+choice = 't'
+
+print("Witaj w programie do odwracania stringów")
+
+while choice == 't':
+
+    zdanie = input("Podaj zdanie które chcesz odwrócić")
+
+    for i in range(1, len(zdanie)+1):
+        print(zdanie[-i])
+
+    choice = input("Czy chcesz podać odwrócić inne zdanie? (t/n)")
